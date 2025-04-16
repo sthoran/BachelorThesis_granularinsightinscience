6636f3bbc35bab83a56bfee7f1c832f5da3811a6
sthoran <stonia.thorand@gmail.com>
2025-04-16 11:02:59 +0200

    update

--- preprocess.py
+++ preprocess.py
@@ -1,52 +1,97 @@
 #%%
 import sqlite3
-import scispacy
-import spacy
+
 import pandas as pd
 import seaborn as sns
 import re
-#%%
+import re
+import unicodedata
+from nltk.tokenize import sent_tokenize
+import nltk
+nltk.download('punkt')
+
+
 # Connect to SQLite
-con = sqlite3.connect("papers.db")
+class Data_cleanup:
+    def __init__(self,data):
+        self.df = data
+    
+    # def get_df_sqlite(self):
+    #     con = sqlite3.connect('papers.db')
+    #     self.df = pd.read_sql_query("SELECT doi, title, abstract, language FROM paper", con)
+    #     con.close()
+    #     return self.df
 
-# Read only abstracts (and maybe titles too)
-df = pd.read_sql_query("SELECT doi, title, abstract, language FROM paper", con)
-#%%
-# Combine title + abstract if useful
-df["text"] = df["title"].fillna('') + ". " + df["abstract"].fillna('')
-# %%
-df['language'].unique()
-# %%
-df = df.drop_duplicates(subset="doi")
-# %%
-df.describe()
-# %%
-sns.histplot(df, x= 'language')
-# %%
-df = df[df['language'] != 'de']
-df.describe()
-# %%
-def preprocess_for_biobert_ner(text):
-    if not text:
-        return ""
-    
-    # Remove HTML tags
-    text = re.sub(r"<.*?>", "", text)
-    
-    # Remove URLs
-    text = re.sub(r"http\S+|www\S+", "", text)
-    
-    # Normalize whitespace
-    text = re.sub(r"\s+", " ", text)
-    
-    # Clean up any stray characters
-    text = text.strip()
-    
-    return text
+    def dataframe_cleanup(self):
+        self.df = self.df.drop_duplicates(subset="doi")
+        self.df = self.df[self.df['language'] != 'de']
+        self.df["text"] = self.df["title"].fillna('') + ". " + self.df["abstract"].fillna('')
+        return self.df
+        
+    def save_dataframe(self,filename):
+        self.df.to_csv(filename, index = False)
 
-# %%
-df["cleaned_text"] = df["text"].fillna("").apply(preprocess_for_ner)
 
-# %%
-nlp = spacy.load("en_ner_bionlp13cg_md")
-# %%
+class Preprocess:
+    def __init__(self,df: pd.DataFrame, column: str):
+        self.df = df.copy()
+        self.column = column
+        self.base_col = self.df[column]
+    
+    
+    def preprocess_for_biobert_gene_ner(self, new_col_name = 'biobert_gene_text'):
+        
+        # Remove HTML tags
+        self.df[new_col_name] = re.sub(r"<.*?>", "", self.base_col)
+        
+        # Remove URLs
+        self.df[new_col_name] = re.sub(r"http\S+|www\S+", "", self.base_col)
+        
+        # Normalize whitespace
+        self.df[new_col_name] = re.sub(r"\s+", " ", self.base_col)
+        
+        # Clean up any stray characters
+        self.df[new_col_name] = self.base_col.strip()
+        
+        return self.df
+    
+    def preprocess_for_biobert_disease_or_chemical_ner(self, new_column = 'biobert_disease_chemical_text'):
+
+        # Remove HTML tags
+        self.df[new_column] = re.sub(r"<.*?>", "", self.base_col)
+
+        # Remove URLs
+        self.df[new_column] = re.sub(r"http\S+|www\S+", "", self.base_col)
+
+        # Normalize Unicode (e.g., replace smart quotes, special symbols)
+        self.df[new_column] = unicodedata.normalize("NFKD", self.base_col)
+
+        # Normalize Greek letters manually if needed
+        greek_replacements = {
+            '\u03b1': 'alpha',  # α
+            '\u03b2': 'beta',   # β
+            '\u03b3': 'gamma',
+            '\u03b4': 'delta',
+        }
+        for k, v in greek_replacements.items():
+            self.df[new_column] = self.base_col.replace(k, v)
+
+        # Normalize whitespace
+        self.df[new_column] = re.sub(r"\s+", " ", self.base_col)
+
+        # Trim
+        self.df[new_column] = self.base_col.strip()
+
+        # Sentence splitting (important for context-sensitive NER)
+        self.df[new_column] = self.df[new_column].apply(sent_tokenize)
+
+        return self.df  # Return list of clean sentences
+    
+    
+    def save_dataframe(self,filename):
+        self.df.to_csv(filename, index = False)
+    
+        
+
+print('test')
+
